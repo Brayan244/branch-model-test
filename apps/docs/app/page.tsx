ca11e28b04c5ba5b89f73f1f8b016f2d39a83c31
Brayan Zavala <zavalabrayan244@gmail.com>
2025-10-21 14:31:10 -0600

    Adds changes

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import Image, { type ImageProps } from 'next/image';
+import { Button } from '@repo/ui/button';
+import styles from './page.module.css';
 
-type Props = Omit<ImageProps, "src"> & {
+type Props = Omit<ImageProps, 'src'> & {
   srcLight: string;
   srcDark: string;
 };
@@ -35,6 +35,7 @@ export default function Home() {
           <li>
             Get started by editing <code>apps/docs/app/page.tsx</code>
           </li>
+          <li>New version</li>
           <li>Save and see your changes instantly.</li>
         </ol>
 
@@ -73,13 +74,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/window.svg" alt="Window icon" width={16} height={16} />
           Examples
         </a>
         <a
@@ -87,13 +82,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Image aria-hidden src="/globe.svg" alt="Globe icon" width={16} height={16} />
           Go to turborepo.com →
         </a>
       </footer>
